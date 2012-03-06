04c6ca45c3b4108e8841cd3eaa769f1333dd053b
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-03-06 01:35:24 -0400

    Disable the graphicsmagick backend for the moment

--- settings_local.py
+++ settings_local.py
@@ -1,3 +1,3 @@
-CONVERTER_GRAPHICS_BACKEND='converter.backends.graphicsmagick'
-CONVERTER_GM_SETTINGS = u'-density 150'
+#CONVERTER_GRAPHICS_BACKEND='converter.backends.graphicsmagick'
+#CONVERTER_GM_SETTINGS = u'-density 150'
 
